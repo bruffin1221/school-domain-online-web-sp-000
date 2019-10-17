febeaec0c8c663e49473a24348b6f5491835bca6
Bruce Ruffin <bruffin1221@gmail.com>
2019-10-17 20:06:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+attr_reader :roster
+
+def initialize(roster)
+  roster={}
+  @roster=roster
+end
+
+def add_student(name, grade)
+     @roster[grade] ||= []
+     @roster[grade] << name
+ end
+
+ def grade(grade)
+   @grade=@roster[grade]
+ end
+
+ def sort
+   @sort=@roster.sort.to_h.sort_by{|k,v| v}
+ end
+
+end
